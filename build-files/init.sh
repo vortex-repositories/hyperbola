66f34ffbbaefab8b5fbea5265f64e13718eaca66
Voidus X <voidus-git@mail.voidus.dev>
2025-01-10 02:57:15 -0800

    fix: issue where a new directory is created from tar extraction

--- build-files/init.sh
+++ build-files/init.sh
@@ -27,7 +27,7 @@ regreet_metadata=$(cat /tmp/regreet/release-metadata)
 regreet_version=$(echo "$regreet_metadata" | jq '.name' | tr -d '"')
 regreet_tarball=$(echo "$regreet_metadata" | jq '.tarball_url')
 wget "https://api.github.com/repos/rharish101/ReGreet/tarball/$regreet_version" -O '/tmp/regreet/repository.tar.gz'
-tar -xvf /tmp/regreet/repository.tar.gz -C /tmp/regreet/repository
+tar -xvf /tmp/regreet/repository.tar.gz -C /tmp/regreet/repository --strip-components=1
 cd /tmp/regreet/repository
 echo "Building Display Manager"
 cargo build -F gtk4_8 --release
